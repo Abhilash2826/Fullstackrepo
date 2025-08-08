63ecbe34c28adfde8d392191c80c7f066afe8378
sadhu <sadhu@Abhi>
2025-08-08 22:16:41 +0530

    commit and push

---
+++ mavenproj01/src/main/java/nit/mavenproj01/App.java
@@ -0,0 +1,12 @@
+package nit.mavenproj01;
+
+public class App {
+    public static void main(String[] args) {
+    	int a=10;
+		int b=20;
+		int  c=a+b;
+		System.out.println("sum::"+c);
+       
+        
+    }
+}
